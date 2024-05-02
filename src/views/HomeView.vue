321e6a1fd4d114d5b3ff602b959cd39f70a64c40
Kauã Nascimento <kauasilva2861>
2024-05-02 07:20:23 -0300

    Novas aulas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
     <!-- Aula 1 -->
     <!-- Renderiza o cabeçalho se a variável showHeader for verdadeira -->
+    <h1>Aula 1</h1>
+
     <TheHeader v-if="showHeader" />
 
 
@@ -11,6 +13,9 @@
 
     <!-- Aula 2 -->
     <!-- Mostra o nome e sobrenome se a variável showName for verdadeira -->
+    <hr>
+    <h1>Aula 2</h1>
+
     <div v-show="showName">
         Nome: {{ nome }}
         <br>
@@ -25,6 +30,9 @@
 
     <!-- Aula 3 -->
     <!-- Condicional que exibe diferentes mensagens dependendo do valor da variável nivelAcesso -->
+    <hr>
+    <h1>Aula 3</h1>
+
     <div v-if="nivelAcesso === 'admin'">Usuário Admin</div>
     <div v-else-if="nivelAcesso === 'marketing'">Usuário Marketing</div>
     <div v-else>Usuário normal</div>
@@ -37,6 +45,9 @@
 
     <!-- Aula 4 -->
     <!-- Imagem com src dinâmico e estilo inline -->
+    <hr>
+    <h1>Aula 4</h1>
+
     <img :src="iconSrc" :alt="iconAlt" style="width: 50px; height: 50px;">
 
 
@@ -47,16 +58,11 @@
 
     <!-- Aula 5 -->
     <!-- Iteração sobre a lista de objetos 'todos' -->
-    <div
-        v-for="obj in todos"
-        :key="obj.id"
-        class="todos-item"
-    >
-        <img
-            v-if="obj.imgSrc"
-            :src="obj.imgSrc"
-            :alt="obj.imgAlt"
-        >
+    <hr>
+    <h1>Aula 5</h1>
+
+    <div v-for="obj in todos" :key="obj.id" class="todos-item">
+        <img v-if="obj.imgSrc" :src="obj.imgSrc" :alt="obj.imgAlt">
         {{ obj.title }}
     </div>
     <!-- Modo "v-for" que gera o index de um array -->
@@ -65,33 +71,32 @@
     </div> -->
 
 
-    
+
+    <br><br>
+
+
+
+    <!-- Aula 6 -->
+    <!-- Outro tipo de iteração sobre a lista de objetos 'todos' -->
+    <hr>
+    <h1>Aula 6</h1>
+
+    <div v-for="objeto in todos" :key="objeto.id">
+        <img v-if="objeto.imgSrc" :src="objeto.imgSrc" :alt="objeto.imgAlt">
+        {{ objeto.imgAlt }}
+    </div>
+
+
+
     <br><br>
 
 
 
     <!-- Aula 7 -->
-    <!-- Outro tipo de iteração sobre a lista de objetos 'todos' -->
-    <div
-        v-for="objeto in todos"
-        :key="objeto.id"
-    >
-        <img
-            v-if="objeto.imgSrc"
-            :src="objeto.imgSrc"
-            :alt="objeto.imgAlt"
-        >
-        {{ objeto.imgAlt }}
-    </div>
-    
-
-
-    <br><br>
-
-
-
-    <!-- Aula 8 -->
     <!-- Utiliza class binding e style binding para modificar a aparência dos elementos -->
+    <hr>
+    <h1>Aula 7</h1>
+
     <h1 :class="{ 'title': true, 'title-home': isHome }">
         Curso Vue 3
     </h1>
@@ -108,15 +113,15 @@
 
 
 
-    <!-- Aula 9 -->
+    <!-- Aula 8 -->
     <!-- Utiliza v-model para vincular os valores dos campos de entrada e seleção às variáveis do Vue.js e exibe esses valores -->
+    <hr>
+    <h1>Aula 8</h1>
+
     <div>
         <label>Nome</label>
         <br>
-        <input
-            type="text"
-            v-model="nome"
-        >
+        <input type="text" v-model="nome">
         <br>
         {{ nome }}
     </div>
@@ -141,16 +146,8 @@
     <div>
         <label>Escolha</label>
         <br>
-        <input
-            v-model="escolha"
-            type="radio"
-            value="Sim"
-        >Sim
-        <input
-            v-model="escolha"
-            type="radio"
-            value="Não"
-        >Não
+        <input v-model="escolha" type="radio" value="Sim">Sim
+        <input v-model="escolha" type="radio" value="Não">Não
         <br>
         {{ escolha }}
     </div>
@@ -160,10 +157,7 @@
     <div>
         <label>Contrato</label>
         <br>
-        <input
-            v-model="contrato"
-            type="checkbox"
-        > Aceita nossos termos...
+        <input v-model="contrato" type="checkbox"> Aceita nossos termos...
         <br>
         {{ contrato }}
     </div>
@@ -173,16 +167,8 @@
     <div>
         <label>Cores que você mais gosta</label>
         <br>
-        <input
-            v-model="cores"
-            type="checkbox"
-            value="Azul"
-        > Azul
-        <input
-            v-model="cores"
-            type="checkbox"
-            value="Amarelo"
-        > Amarelo
+        <input v-model="cores" type="checkbox" value="Azul"> Azul
+        <input v-model="cores" type="checkbox" value="Amarelo"> Amarelo
         <br>
         {{ cores }}
     </div>
@@ -193,35 +179,118 @@
 
 
 
-    <!-- Aula 10 -->
+    <!-- Aula 9 -->
     <!-- Utiliza eventos de clique, mouseover e mouseout -->
+    <hr>
+    <h1>Aula 9</h1>
+
     <div>
-        <button @click.once="onClick()">
-            Enviar
+        <button @click.stop="onClick()">
+            Enviar (Impede a propagação do evento/Para de propagar o evento)
         </button>
-        <br>
-        <div
-            @mouseover="onMouseOver()"
-            @mouseout="onMouseOut()"    
-        >
+        
+        <br><br>
+
+        <button @click.prevent="onClick()">
+            Enviar (Previne o comportamento padrão/Impede a ação padrão)
+        </button>
+        
+        <br><br>
+
+        <button @click.capture="onClick()">
+            Enviar (Invoca o manipulador durante a fase de captura/Executa antes nos pais)
+        </button>
+        
+        <br><br>
+
+        <button @click.once="onClick()">
+            Enviar (Só é invocado uma vez/Só funciona uma vez)
+        </button>
+        
+        <br><br>
+
+        <button @click.passive="onClick()">
+            Enviar (Otimiza o desempenho/Melhora a resposta)
+        </button>
+
+        <br><br>
+
+        <div @mouseover="onMouseOver()" @mouseout="onMouseOut()">
             Mouse Over
         </div>
 
-        <br>
+        <br><br>
 
-        <form
-            action="https://www.google.com.br/"
-            @submit.prevent="onSubmit"
-        >
-            <input
-                type="text"
-                @keyup.enter="onKeyUp"    
-            >
+        <form action="https://www.google.com.br/" @submit.prevent="onSubmit">
+            <input type="text" @keyup.enter="onKeyUp">
             <button type="submit">
                 Enviar
             </button>
         </form>
     </div>
+
+
+
+    <br><br>
+
+
+
+    <!-- Aula 10 -->
+    <hr>
+    <h1>Aula 10</h1>
+
+
+
+    <br><br>
+
+
+
+    <!-- Aula 11 -->
+    <!-- Utiliza observadores (watch) -->
+    <hr>
+    <h1>Aula 11</h1>
+    <div>
+        <input
+            type="text"
+            v-model="nome"
+        > <br>
+        {{ nome }}
+
+        <br><br>
+
+        <select v-model="contagemPagina">
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+        </select>
+        <br>
+        {{ contagemPagina }}
+
+        <br><br>
+
+        <input
+            v-model="usuario.primeiro_nome"
+            type="text"
+        >
+        <br>
+        <input
+            v-model="usuario.segundo_nome"
+            type="text"
+        >
+        <br>
+        {{ usuario.primeiro_nome }}
+        <br>
+        {{ usuario.segundo_nome }}
+    </div>
+
+
+
+    <br><br>
+
+
+
+    <!-- Aula 12 -->
+    <!-- ... -->
 </template>
 
 <script>
@@ -299,7 +368,16 @@ export default {
             esportes: '',
             escolha: '',
             contrato: true,
-            cores: []
+            cores: [],
+
+
+
+            //Aula 11
+            contagemPagina: 5,
+            usuario: {
+                primeiro_nome: "Kauã",
+                segundo_nome: "Nascimento da Silva"
+            }
         }
     },
 
@@ -323,6 +401,38 @@ export default {
 
         onKeyUp(evento) {
             console.log('onKeyUp'), evento;
+        },
+
+
+
+        // Aula 11
+        salvarNomeUsuario() {
+            console.log('Ajax');
+            console.log(this.nome);
+        },
+
+        mudarPagina() {
+            console.log('Ajax mudar de paginas');
+        }
+    },
+
+    watch: {
+        // Aula 11
+        nome(newValue) {
+            if (newValue.length >= 3) {
+                this.salvarNomeUsuario()
+            }
+        },
+
+        contagemPagina() {
+            this.mudarPagina()            
+        },
+
+        usuario: {
+            handler() {
+                console.log('Usuário Alterado');
+            },
+            deep: true
         }
     }
 }
