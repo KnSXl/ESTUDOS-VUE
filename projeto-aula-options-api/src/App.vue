97e1e23c911687414c2aaca896f92f6a1bb02ca6
Kauã Nascimento <kauasilva2861>
2024-05-14 22:34:45 -0300

    Projeto em aula com options api

---
+++ projeto-aula-options-api/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <!-- Content -->
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+
+            <TodoSpinner v-if="loading" />
+
+            <template v-else>
+                <TodoFormAdd />
+
+                <TodoItems />
+
+                <TodoEmpty />
+            </template>
+        </div>
+    </div>
+    <!--/ Content -->
+</template>
+
+<script>
+import TodoSpinner from './components/TodoSpinner.vue';
+import TodoFormAdd from './components/TodoFormAdd.vue';
+import TodoItems from './components/TodoItems.vue';
+import TodoEmpty from './components/TodoEmpty.vue';
+import axios from 'axios';
+
+export default {
+    components: {
+        TodoSpinner,
+        TodoFormAdd,
+        TodoItems,
+        TodoEmpty
+    },
+
+    data() {
+        return {
+            todos: [],
+            loading: false,
+        }
+    },
+
+    async created() {
+        this.loading = true
+        this.todos = await axios.get('http://localhost:3000/todos')
+            .then((response) => {
+                this.$store.commit('storeTodos', response.data)
+            })
+            .finally(() => {
+                this.loading = false
+            })
+    },
+}
+</script>
+
+<style scoped></style>
